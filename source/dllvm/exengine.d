d6f1f1850197abcbe89f943493e0acc09e1a6a89
Member1221 <clipseypone@gmail.com>
2019-10-02 10:46:19 +0200

    MCJIT support

--- source/dllvm/exengine.d
+++ source/dllvm/exengine.d
@@ -1,12 +1,29 @@
 module dllvm.exengine;
+import std.traits, std.string, std.algorithm.mutation, std.algorithm.searching;
 import dllvm;
 import llvm;
-import std.traits;
+
+version (Windows) {
+    enum USE_MC_JIT_DEFAULT = false;
+} else {
+    enum USE_MC_JIT_DEFAULT = true;
+}
+
+/**
+    Initialize things needed for the execution engine
+*/
+void initExecutionEngine() {
+    LLVMInitializeNativeTarget();
+    LLVMInitializeNativeAsmPrinter();
+}
 
 /**
     The LLVM Execution Engine
 */
 class ExecutionEngine {
+private:
+    Module[] managedModules;
+
 package(dllvm):
 
     /// Hidden constructor for backend uses.
@@ -23,9 +40,24 @@ public:
     /**
         Initialized execution engine for module
     */
-    this(Module mod) {
-        char** errPtr = new char*;
-        LLVMCreateExecutionEngineForModule(&ptr, mod.ptr, errPtr);
+    this(Module mod, bool useMCJIT = true) {
+        char* error;
+        if (useMCJIT) {
+            LLVMMCJITCompilerOptions options;
+            LLVMInitializeMCJITCompilerOptions(&options, options.sizeof);
+
+            LLVMCreateMCJITCompilerForModule(&ptr, mod.ptr, &options, options.sizeof, &error);
+        } else {
+            LLVMCreateExecutionEngineForModule(&ptr, mod.ptr, &error);
+        }
+
+        if (error) {
+            scope (exit) LLVMDisposeMessage(error);
+            throw new Exception(error.fromStringz().idup);
+        }
+
+        // Add to the managed modules list.
+        managedModules ~= mod;
     }
 
     ~this() {
@@ -49,6 +81,45 @@ public:
     */
     void AddModule(Module mod) {
         LLVMAddModule(ptr, mod.ptr);
+        managedModules ~= mod;
+    }
+
+    /**
+        Removes the module from the execution engine
+    */
+    void RemoveModule(Module mod) {
+        char* error;
+        LLVMRemoveModule(ptr, mod.ptr, &mod.ptr, &error);
+        if (error) throw new Exception(error.fromStringz.idup());
+
+        managedModules.remove(managedModules.countUntil(mod));
+    }
+
+    /**
+        Recompiles all modules
+    */
+    void RecompileAll() {
+        // Copy the managed modules list
+        Module[] modules = managedModules;
+
+        foreach(module_; modules) {
+            Recompile(module_);
+        }
+    }
+
+    /**
+        Recompiles a module
+    */
+    void Recompile(Module mod) {
+        RemoveModule(mod);
+        AddModule(mod);
+    }
+
+    /**
+        Returns the function address of the specified function
+    */
+    T GetFunctionAddr(T)(string name) {
+        return cast(T)LLVMGetFunctionAddress(ptr, name.toStringz);
     }
 }
 
